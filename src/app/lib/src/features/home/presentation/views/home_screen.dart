d9f4687bd4037ede1324e6094e58e374cd590082
NHV Nha <nguyenhoangvannha@gmail.com>
2024-03-15 22:16:54 +0700

    Impl responsive scaffold

--- src/app/lib/src/features/home/presentation/views/home_screen.dart
+++ src/app/lib/src/features/home/presentation/views/home_screen.dart
@@ -48,96 +48,115 @@ class _HomePageState extends State<HomePage> {
     // Define the list of destinations to be used within the app.
     List<NavigationDestination> destinations = _destinations();
 
-    // #docregion Example
-    // AdaptiveLayout has a number of slots that take SlotLayouts and these
-    // SlotLayouts' configs take maps of Breakpoints to SlotLayoutConfigs.
-    return AdaptiveLayout(
-      // An option to override the default transition duration.
-      transitionDuration: Duration(milliseconds: _transitionDuration),
-      // Primary navigation config has nothing from 0 to 600 dp screen width,
-      // then an unextended NavigationRail with no labels and just icons then an
-      // extended NavigationRail with both icons and labels.
-      primaryNavigation: SlotLayout(
-        config: <Breakpoint, SlotLayoutConfig>{
-          Breakpoints.medium: SlotLayout.from(
-            inAnimation: AdaptiveScaffold.leftOutIn,
-            key: const Key('Primary Navigation Medium'),
-            builder: (_) => AdaptiveScaffold.standardNavigationRail(
-              selectedIndex: selectedNavigation,
-              onDestinationSelected: (int newIndex) {
-                setState(() {
-                  selectedNavigation = newIndex;
-                });
-              },
-              leading: const Icon(Icons.menu),
-              destinations: destinations
-                  .map((NavigationDestination destination) =>
-                      AdaptiveScaffold.toRailDestination(destination))
-                  .toList(),
-              backgroundColor: navRailTheme.backgroundColor,
-              selectedIconTheme: navRailTheme.selectedIconTheme,
-              unselectedIconTheme: navRailTheme.unselectedIconTheme,
-              selectedLabelTextStyle: navRailTheme.selectedLabelTextStyle,
-              unSelectedLabelTextStyle: navRailTheme.unselectedLabelTextStyle,
-            ),
-          ),
-          Breakpoints.large: SlotLayout.from(
-            key: const Key('Primary Navigation Large'),
-            inAnimation: AdaptiveScaffold.leftOutIn,
-            builder: (_) => AdaptiveScaffold.standardNavigationRail(
-              selectedIndex: selectedNavigation,
-              onDestinationSelected: (int newIndex) {
-                setState(() {
-                  selectedNavigation = newIndex;
-                });
-              },
-              extended: true,
-              leading: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  const Icon(Icons.account_circle_rounded),
-                  Text(
-                    context.l10n.authorName,
-                    style: const TextStyle(
-                        color: Color.fromARGB(255, 255, 201, 197)),
-                  ),
-                ],
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(kToolbarHeight),
+        child: SlotLayout(
+          config: <Breakpoint, SlotLayoutConfig>{
+            Breakpoints.small: SlotLayout.from(
+              key: const Key('Top Navigation Small'),
+              outAnimation: AdaptiveScaffold.bottomToTop,
+              builder: (_) => appBar(),
+            )
+          },
+        ),
+      ),
+      // #docregion Example
+      // AdaptiveLayout has a number of slots that take SlotLayouts and these
+      // SlotLayouts' configs take maps of Breakpoints to SlotLayoutConfigs.
+      body: AdaptiveLayout(
+        // An option to override the default transition duration.
+        transitionDuration: Duration(milliseconds: _transitionDuration),
+        // Primary navigation config has nothing from 0 to 600 dp screen width,
+        // then an unextended NavigationRail with no labels and just icons then an
+        // extended NavigationRail with both icons and labels.
+        primaryNavigation: SlotLayout(
+          config: <Breakpoint, SlotLayoutConfig>{
+            Breakpoints.medium: SlotLayout.from(
+              inAnimation: AdaptiveScaffold.leftOutIn,
+              key: const Key('Primary Navigation Medium'),
+              builder: (_) => AdaptiveScaffold.standardNavigationRail(
+                selectedIndex: selectedNavigation,
+                onDestinationSelected: (int newIndex) {
+                  setState(() {
+                    selectedNavigation = newIndex;
+                  });
+                },
+                leading: const Icon(Icons.menu),
+                destinations: destinations
+                    .map((NavigationDestination destination) =>
+                        AdaptiveScaffold.toRailDestination(destination))
+                    .toList(),
+                trailing: Column(
+                  children: [
+                    Divider(),
+                    buildSettingIcon(),
+                  ],
+                ),
+                backgroundColor: navRailTheme.backgroundColor,
+                selectedIconTheme: navRailTheme.selectedIconTheme,
+                unselectedIconTheme: navRailTheme.unselectedIconTheme,
+                selectedLabelTextStyle: navRailTheme.selectedLabelTextStyle,
+                unSelectedLabelTextStyle: navRailTheme.unselectedLabelTextStyle,
               ),
-              destinations: destinations
-                  .map((NavigationDestination destination) =>
-                      AdaptiveScaffold.toRailDestination(destination))
-                  .toList(),
-              trailing: trailingNavRail(),
-              backgroundColor: navRailTheme.backgroundColor,
-              selectedIconTheme: navRailTheme.selectedIconTheme,
-              unselectedIconTheme: navRailTheme.unselectedIconTheme,
-              selectedLabelTextStyle: navRailTheme.selectedLabelTextStyle,
-              unSelectedLabelTextStyle: navRailTheme.unselectedLabelTextStyle,
             ),
-          ),
-        },
-      ),
-      // Body switches between a ListView and a GridView from small to medium
-      // breakpoints and onwards.
-      body: SlotLayout(
-        config: <Breakpoint, SlotLayoutConfig>{
-          Breakpoints.small: SlotLayout.from(
-            key: const Key('Body Small'),
-            builder: (_) => ListView.builder(
-              itemCount: children.length,
-              itemBuilder: (BuildContext context, int index) => children[index],
+            Breakpoints.large: SlotLayout.from(
+              key: const Key('Primary Navigation Large'),
+              inAnimation: AdaptiveScaffold.leftOutIn,
+              builder: (_) => AdaptiveScaffold.standardNavigationRail(
+                selectedIndex: selectedNavigation,
+                onDestinationSelected: (int newIndex) {
+                  setState(() {
+                    selectedNavigation = newIndex;
+                  });
+                },
+                extended: true,
+                leading: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    const Icon(Icons.account_circle_rounded),
+                    Text(
+                      context.l10n.authorName,
+                      style: const TextStyle(
+                          color: Color.fromARGB(255, 255, 201, 197)),
+                    ),
+                  ],
+                ),
+                destinations: destinations
+                    .map((NavigationDestination destination) =>
+                        AdaptiveScaffold.toRailDestination(destination))
+                    .toList(),
+                trailing: trailingNavRail(),
+                backgroundColor: navRailTheme.backgroundColor,
+                selectedIconTheme: navRailTheme.selectedIconTheme,
+                unselectedIconTheme: navRailTheme.unselectedIconTheme,
+                selectedLabelTextStyle: navRailTheme.selectedLabelTextStyle,
+                unSelectedLabelTextStyle: navRailTheme.unselectedLabelTextStyle,
+              ),
             ),
-          ),
-          Breakpoints.mediumAndUp: SlotLayout.from(
-            key: const Key('Body Medium'),
-            builder: (_) =>
-                GridView.count(crossAxisCount: 2, children: children),
-          )
-        },
+          },
+        ),
+        // Body switches between a ListView and a GridView from small to medium
+        // breakpoints and onwards.
+        body: SlotLayout(
+          config: <Breakpoint, SlotLayoutConfig>{
+            Breakpoints.small: SlotLayout.from(
+              key: const Key('Body Small'),
+              builder: (_) => ListView.builder(
+                itemCount: children.length,
+                itemBuilder: (BuildContext context, int index) =>
+                    children[index],
+              ),
+            ),
+            Breakpoints.mediumAndUp: SlotLayout.from(
+              key: const Key('Body Medium'),
+              builder: (_) =>
+                  GridView.count(crossAxisCount: 2, children: children),
+            )
+          },
+        ),
       ),
-      // BottomNavigation is only active in small views defined as under 600 dp
-      // width.
-      bottomNavigation: SlotLayout(
+      bottomNavigationBar: SlotLayout(
         config: <Breakpoint, SlotLayoutConfig>{
           Breakpoints.small: SlotLayout.from(
             key: const Key('Bottom Navigation Small'),
@@ -156,8 +175,9 @@ class _HomePageState extends State<HomePage> {
         },
       ),
     );
-    // #enddocregion Example
   }
+
+  Icon buildSettingIcon() => Icon(Icons.settings);
 
   List<NavigationDestination> _destinations() {
     return <NavigationDestination>[
@@ -206,6 +226,15 @@ class _HomePageState extends State<HomePage> {
           ),
           const SizedBox(height: 10),
           const SettingsPage()
+        ],
+      );
+
+  AppBar appBar() => AppBar(
+        title: Text(context.l10n.authorName),
+        centerTitle: true,
+        actions: [
+          buildSettingIcon(),
+          const SizedBox(width: 10),
         ],
       );
 }
