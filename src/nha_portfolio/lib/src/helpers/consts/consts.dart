c9f60b099b9c44e4ff719ce77e6b3a4b130841a9
NHV Nha <nguyenhoangvannha@gmail.com>
2024-03-17 16:01:43 +0700

    Impl click on flutter icon

--- src/nha_portfolio/lib/src/helpers/consts/consts.dart
+++ src/nha_portfolio/lib/src/helpers/consts/consts.dart
@@ -3,4 +3,6 @@ abstract class Consts {
   static const authorPhone = "+84388889316";
   static const authorGithub = "https://github.com/nguyenhoangvannha";
   static const authorLinkedIn = "https://www.linkedin.com/in/nguyenhoangvannha";
+  static const flutterSite = "https://flutter.dev/";
+  static const dartSite = "https://dart.dev/";
 }
--- src/nha_portfolio/lib/src/presentation/widgets/make_with.dart
+++ src/nha_portfolio/lib/src/presentation/widgets/make_with.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:nha_portfolio/src/assets/generated/assets.gen.dart';
+import 'package:nha_portfolio/src/helpers/consts/consts.dart';
 import 'package:nha_portfolio/src/helpers/ext/context_ext.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MakeWith extends StatelessWidget {
   const MakeWith({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final size  = 28.0;
+    final size = 28.0;
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         Text(context.l10n.makeByAndWith(context.l10n.authorName)),
-        SizedBox(width: 4,),
-        Assets.icons.icons8Flutter48.image(
-          width: size,
-          height: size,
+        SizedBox(
+          width: 4,
         ),
-        Assets.icons.icons8Dart48.image(       width: size,
-          height: size,),
+        IconButton(
+          onPressed: () {
+            launchUrl(Uri.parse(Consts.flutterSite));
+          },
+          icon: Assets.icons.icons8Flutter48.image(
+            width: size,
+            height: size,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            launchUrl(Uri.parse(Consts.dartSite));
+          },
+          icon: Assets.icons.icons8Dart48.image(
+            width: size,
+            height: size,
+          ),
+        ),
       ],
     );
   }
